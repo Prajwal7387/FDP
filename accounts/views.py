c3acac389631e6c2c27b0a88ce864a0449c4b120
Prajwal7387 <prajwalmahamuni@gmail.com>
2026-03-06 19:27:51 +0530

    Practical 6 Django CRUD project

---
+++ accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('login')
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    form = AuthenticationForm(request, data=request.POST or None)
+    if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        return redirect('profile')
+    return render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
